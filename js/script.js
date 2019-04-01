55e2247a78603fe678111a1d55de7033cebb0295
bambooka <sumkina.ol@yandex.ru>
2019-04-01 14:40:11 +0300

    add img and title

--- js/script.js
+++ js/script.js
@@ -1,40 +1,33 @@
 
-var cat1 = document.getElementById('cat1');
-var cat2 = document.getElementById('cat2');
-var counter = document.getElementById('counter');
-var clicks = 0;
-var imgCatRoof = document.createElement('img');
-var imgCatFlower = document.createElement('img');
-
+var listCat = document.querySelectorAll('catsList');
 var cats = [
 
-{name: 'Bob', img: 'cat_flower.jpg'},
-{name: 'Marcel', img: 'cat_roof.jpg'},
-{name: 'Puma', img: 'cat_boquet.jpg'},
-{name: 'Sam', img: 'cat_frog.jpgg'},
-{name: 'Vasy', img: 'cat_dreams.jpg'},
+{name: 'Bob', img: './img/cat_flower.jpg'},
+{name: 'Marcel', img: './img/cat_roof.jpg'},
+{name: 'Puma', img: './img/cat_boquet.jpg'},
+{name: 'Sam', img: './img/cat_frog.jpg'},
+{name: 'Vasy', img: './img/cat_dreams.jpg'},
 ];
 
-var catFlowerName = document.createElement('p');
-catFlowerName.innerHTML = 'Homa';
-var catRoofName = document.createElement('p');
-catRoofName.innerHTML = 'Moma';
-
-imgCatFlower.setAttribute("src", 'cat_flower.jpg');
-imgCatRoof.setAttribute("src", 'cat_roof.jpg');
-
-cat1.appendChild(catFlowerName);
-cat1.appendChild(imgCatFlower);
-
-cat2.appendChild(catRoofName);
-cat2.appendChild(imgCatRoof);
 
 
 
-var increase = function(){
-	clicks++;
-	counter.innerHTML = clicks;
+
+console.log(cats);
+console.log(listCat);
+
+cats.forEach(function(cat){
+	console.log(cat);
+	let containerCat = document.createElement('div');
+	let catName = document.createElement('p');
+	catName.innerHTML = cat.name; 
+	let catImg = document.createElement('img');
+	catImg.setAttribute('src', cat.img);
+	catImg.style.width = '200px';	
+
+	containerCat.appendChild(catName);
+	containerCat.appendChild(catImg);
+	document.body.appendChild(containerCat);
+	
 }
-
-cat1.addEventListener('click', increase, false);
-cat2.addEventListener('click', increase, false);
+)
